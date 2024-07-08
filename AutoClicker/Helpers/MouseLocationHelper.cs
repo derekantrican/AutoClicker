c0b37815c33a4a1d327c5e31e5e59d8b2f9ea099
Derek Antrican <derekantrican@gmail.com>
2024-07-08 09:44:55 -0700

    Removed unnecessary user32 call

--- AutoClicker/Helpers/MouseLocationHelper.cs
+++ AutoClicker/Helpers/MouseLocationHelper.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Drawing;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using Gma.System.MouseKeyHook;
 
@@ -15,39 +14,9 @@ namespace AutoClicker.Helpers
             Hook.GlobalEvents().MouseClick += MouseLocationHelper_MouseClick;
         }
 
-        [StructLayout(LayoutKind.Sequential)]
-        public struct POINT
-        {
-            public int X;
-            public int Y;
-
-            public POINT(int x, int y)
-            {
-                this.X = x;
-                this.Y = y;
-            }
-
-            public static implicit operator Point(POINT p)
-            {
-                return new Point(p.X, p.Y);
-            }
-
-            public static implicit operator POINT(Point p)
-            {
-                return new POINT(p.X, p.Y);
-            }
-        }
-
         private static void MouseLocationHelper_MouseClick(object sender, MouseEventArgs e)
         {
-            if (GetCursorPos(out POINT p))
-            {
-                LocationChangeCallback?.Invoke(new Point(p.X, p.Y));
-            }
+            LocationChangeCallback?.Invoke(new Point(e.X, e.Y));
         }
-
-        [DllImport("user32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool GetCursorPos(out POINT lpPoint);
     }
 }
